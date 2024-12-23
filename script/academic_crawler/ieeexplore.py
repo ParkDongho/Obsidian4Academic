35c53e51015b5f2252adfc2263e48fa26549de10
parkdongho <parkdongho28@naver.com>
2024-12-23 16:08:44 +0900

    update

--- script/academic_crawler/ieeexplore.py
+++ script/academic_crawler/ieeexplore.py
@@ -40,34 +40,96 @@ def parseSection(input):
                         section_title = heading_tag.text.strip()
                         break
 
-            # 본문 처리
-            elif paragraph.name == "p":
-                # Replace inline and display formulas with Markdown-friendly syntax
-                for math in paragraph.find_all('inline-formula'):
-                    latex_code = math.find('tex-math').text if math.find('tex-math') else ""
-                    math.replace_with(f"${latex_code}$")
-
-                # link 처리
-
-                paragraph_text = ' '.join(paragraph.stripped_strings)
-                section_content += paragraph_text + "\n\n"
-
             elif paragraph.name == "disp-formula":
                 # 찾고자 하는 <span> 태그와 그 내부의 텍스트를 추출
                 span_tag = paragraph.find('span', class_="tex tex2jax_ignore")
                 if span_tag and span_tag.text:
                     latex_code = span_tag.text.strip()  # 텍스트를 가져와서 공백 제거
-                    section_content += f"$$\n{latex_code}\n$$\n\n"
+                    section_content += f"\n$$\n{latex_code}\n$$\n\n"
+                else:
+                    print("No latex code found in disp-formula")
 
-            # Figure 처리
+            # 본문 처리
+            elif paragraph.name == "p":
+
+                # inline-formula 처리
+                for math in paragraph.find_all('inline-formula'):
+                    latex_code = math.find('script', {'type': 'math/tex'}).text
+                    math.replace_with(f"${latex_code}$")
+
+                for math in paragraph.find_all('disp-formula'):
+                    span_tag = math.find('span', class_="tex tex2jax_ignore")
+                    if span_tag and span_tag.text:
+                        latex_code = span_tag.text.strip()  # 텍스트를 가져와서 공백 제거
+                        math.replace_with(f"\n$$\n{latex_code}\n$$\n\n")
+                    else:
+                        print("No latex code found in disp-formula")
+
+                # 참조 처리
+                for link in paragraph.find_all('a'):
+                    # bibriography 처리
+                    if link.attrs['ref-type'] == "bibr":
+                        link_text = link.text
+                        if link.text[0] == "[":
+                            link_text = link.text.replace("[", "\[")
+                        if link.text[-1] == "]":
+                            link_text = link_text.replace("]", "\]")
+                        link.replace_with(f"[{link_text}]({link.attrs['anchor']})")
+                    # 본문 내 이미지 관련 참조 처리
+                    elif link.attrs['ref-type'] == "fig":
+                        link.replace_with(f"[{link.text}]({link.attrs['anchor']})")
+                    # 본문 내 표 관련 참조 처리
+                    elif link.attrs['ref-type'] == "table":
+                        link.replace_with(f"[{link.text}]({link.attrs['anchor']})")
+                    # 본문 내 섹션 관련 참조 처리
+                    elif link.attrs['ref-type'] == "sec":
+                        link.replace_with(f"[{link.text}]({link.attrs['anchor']})")
+                    # 본문 내 수식 관련 참조 처리
+                    elif link.attrs['ref-type'] == "fn":
+                        link.replace_with(f"[{link.text}]({link.attrs['anchor']})")
+                    else:
+                        print("Unhandled Link: ", link.text, ", ", link.attrs['ref-type'], ", ", link.attrs['anchor'])
+
+                paragraph_text = ' '.join(paragraph.stripped_strings)
+                section_content += paragraph_text + "\n\n"
+
+
+            # Figure 생성
             elif any(cls in paragraph.get('class', []) for cls in ["figure", "figure-full"]):
-                print("figure")
+                # 이미지와 캡션 파싱
+                image_wrap = paragraph.find('div', class_='img-wrap')
+                fig_caption = paragraph.find('div', class_='figcaption')
 
-            # Table 처리
+                if image_wrap and fig_caption:
+                    # 링크와 이미지 경로 추출
+                    link_tag = image_wrap.find('a')
+                    img_tag = link_tag.find('img') if link_tag else None
+                    image_href = link_tag['href'] if link_tag else ''
+                    alt_text = img_tag.get('alt', 'Image') if img_tag else ''
+
+                    # 캡션 추출
+                    caption_title = fig_caption.find('b', class_='title').get_text(strip=True) if fig_caption.find('b',
+                                                                                                                   class_='title') else ''
+                    caption_text = fig_caption.find('p').get_text(strip=True) if fig_caption.find('p') else ''
+
+                    # 마크다운 형식으로 변환
+                    markdown_output = f"![{alt_text}]({image_href})\n\n**{caption_title}** {caption_text}"
+
+                    # 섹션 내용에 추가
+                    section_content += f"\n{markdown_output}\n"
+
+                else:
+                    section_content += "\nFigure could not be parsed.\n"
+
+
+            # Table 생성
+            elif any(cls in paragraph.get('class', []) for cls in ["table", "table-full"]):
+                print("table")
 
 
             else:
                 print("\n\nUnhandled Tag:", paragraph)
+
 
         single_section = [(heading_level, section_title, section_content)] + subsec_list
 
